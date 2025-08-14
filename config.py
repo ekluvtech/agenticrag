1f29ba7f620b18c060bcb062537d24dabc149b97
ekluvtech <techekluv@gmail.com>
2025-08-14 22:16:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ LLM_MODEL = os.getenv('LLM_MODEL','llama3.2')
 EMBED_MODEL = os.getenv('EMBED_MODEL','mxbai-embed-large:latest')
 FAISS_INDEX_NAME= os.getenv('FAISS_INDEX_NAME','faiss_idx')
 # vector store config
-FOLDER_PATH = os.getenv('FOLDER_PATH','/Users/srikanth/Documents/ekluv2/faissdata/data')
-INDEX_STORAGE_PATH = os.getenv('INDEX_STORAGE_PATH','/Users/srikanth/Documents/ekluv2/faissdata/index')
+FOLDER_PATH = os.getenv('FOLDER_PATH','<<ROOT_DIRECTORY>>/faissdata/data')
+INDEX_STORAGE_PATH = os.getenv('INDEX_STORAGE_PATH',''<<ROOT_DIRECTORY>>/faissdata/index')
